ad865a1d6aa0d9087e73a83ae9ccf475036c235b
DG1YIQ <marco.reinke@me.com>
2020-07-15 21:26:18 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,143 @@
+/*
+ 
+ ATMega8 ADF4110 PLL
+
+ MIT License
+
+ Copyright (c) 2020 Marco Reinke, DG1YIQ
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+*/
+
+#include "main.h"
+
+//Hauptprogramm
+int main(void)
+{
+    LED_BEAT_DDR |= (1 << LED_BEAT_PIN);        // Beat LED Output
+    LED_BEAT_PORT &= ~(1 << LED_BEAT_PIN);      //Beat LED aus
+    
+    //SPI Pins initialisieren
+    spiinit();
+    
+    //PLL initialisieren
+    pllinit(Frequenz);
+    
+    //
+    //Hauptschleife
+    //
+    
+    while(1)
+    {
+        //Hier tun wir nicht, außer ein bischen geblinke :-)
+        LED_BEAT_PORT |= (1 << LED_BEAT_PIN);   //LED an
+        _delay_ms(100);
+        LED_BEAT_PORT &= ~(1 << LED_BEAT_PIN);  //LED aus
+        _delay_ms(100);
+        LED_BEAT_PORT|= (1 << LED_BEAT_PIN);    //LED an
+        _delay_ms(100);
+        LED_BEAT_PORT&= ~(1 << LED_BEAT_PIN);   //LED aus
+        _delay_ms(700);
+        //ggf den MC in den Sleep Mode versetzen, aus dem er nicht wieder aufwacht, um Störungen durch diesen zu minimieren.
+        /*
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);    //Sleep Mode definieren
+        sleep_mode();                           //Sleep Mode aktivieren
+        */
+	}
+    return 0;
+}
+
+void spiinit(void)
+{
+    //Alle SPI Pin's als Ausgang definieren
+    //Die Zuordnung zu den HW Pins erfolgt in der main.h
+    SPI_MOSI_DDR    |= (1 << SPI_MOSI_PIN);
+    SPI_SCK_DDR     |= (1 << SPI_SCK_PIN);
+    SPI_ENA_DDR     |= (1 << SPI_ENA_PIN);
+    
+    //Alles Pins Low
+    SPI_MOSI_PORT   &= ~(1 << SPI_MOSI_PIN);    //Data Low
+    SPI_SCK_PORT    &= ~(1 << SPI_SCK_PIN);     //Clock Low
+    SPI_ENA_PORT    &= ~(1 << SPI_ENA_PIN);     //Enable Low
+    return;
+}
+
+void pllinit(uint32_t freq)
+{
+    uint32_t    MUX=MuxPin;
+    uint32_t    R;
+    uint32_t    A;
+    uint32_t    B;
+    
+    //Brechne Register R,A und B anhand der in der main.h festgelegten Parameter
+    R=Referenz/Step;
+    B=(freq/Step)/Prescaler;
+    A=(freq/Step)-(Prescaler*B);
+    
+    //Kurze Pause
+    _delay_ms(1);
+    
+    //Übertragung der Register an den ADF4110 - Wichtig: zum schreiben der Register muss der ADF im Counter/Reset Modus sein.
+    spiwrite((Prescaler_Reg<<R2_Prescaler) | (MUX<<R2_MUX) | (1<<R2_Counter_Reset) | (1<<R2_PD_Polarity) | 0x2UL );     //R2 - Counter Reset = 1
+    spiwrite((R<<R0_R_Counter) | 0x0UL );                                                                               //R0 - R-Counter
+    spiwrite((B<<R1_B_Counter) | (A<<R1_A_Counter) | 0x1UL );                                                           //R1 - A+B-Counter
+    spiwrite((Prescaler_Reg<<R2_Prescaler) | (MUX<<R2_MUX) | (1<<R2_PD_Polarity) | 0x2UL);                              //R2 - Counter Reset = 0
+}
+
+void spiwrite(uint32_t byte)
+{
+    //Software SPI Routine - SPI BitBanging
+
+    uint8_t count;
+    
+    //Der ADF4110 hat 24bit große Register - Zähler 0 bis 23
+    for (count=0; count<24 ; count++)
+    {
+        //Prüfe ausmaskiertes DB23 ob 1 oder 0
+        if (byte & (1UL << 23))
+        {
+            //Wenn Bit 1 dann den SPI MOSI Pin High setzen
+            SPI_MOSI_PORT |= (1 << SPI_MOSI_PIN);
+        } else {
+            //Wenn Bit 0 dann den SPI MOSI Pin Low setzen
+            SPI_MOSI_PORT &= ~(1 << SPI_MOSI_PIN);
+        }
+        //Am Ende eines Bites Clock High
+        SPI_SCK_PORT |= (1 << SPI_SCK_PIN); // Clock High
+        //Prüfe ob wir uns am Ende der Übertragung befinden
+        if (count == 23)
+        {
+            //Ende des Bytes/letztes Bit -> kurz SPI ENA Pin HighLow
+            //Achtung ist der Mikrocontroller zu schnell kann der Impuls für den ADF zu "kurz" sein... ggf Delay einfügen
+            SPI_ENA_PORT |= (1 << SPI_ENA_PIN); // Enable High
+            //_delay_us(10);
+            SPI_ENA_PORT &= ~(1 << SPI_ENA_PIN); //Enable Low
+        }
+        //SPI Clock wieder Low
+        SPI_SCK_PORT &= ~(1 << SPI_SCK_PIN); //Clock Low
+        //Rotiere das zu sendende Byte ein Bit nach Links sodass das nächste zu sendende Bit an die Stelle DB23 kommt
+        byte<<=1;
+    }
+    //Ende der Übertragung - SPI MOSI low
+    SPI_MOSI_PORT &= ~(1 << SPI_MOSI_PIN);
+    
+    return;
+}
+
---
+++ main.h
@@ -0,0 +1,79 @@
+#ifndef F_CPU
+#define F_CPU 1000000
+#endif
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <util/delay.h>
+#include <avr/cpufunc.h>
+#include <avr/sleep.h>
+
+//Prototypen
+void spiwrite(uint32_t byte);
+void spiinit(void);
+void pllinit(uint32_t freq);
+//void setfreq(uint32_t freq);
+
+//Definitionen PLL SPI Interface
+#define     SPI_SCK_PORT    PORTB
+#define     SPI_SCK_DDR     DDRB
+#define     SPI_SCK_PIN     PB2
+#define     SPI_MOSI_PORT   PORTB
+#define     SPI_MOSI_DDR    DDRB
+#define     SPI_MOSI_PIN    PB0
+#define     SPI_ENA_PORT    PORTB
+#define     SPI_ENA_DDR     DDRB
+#define     SPI_ENA_PIN     PB3
+
+//Definition LED's
+#define     LED_BEAT_PORT   PORTB
+#define     LED_BEAT_DDR    DDRB
+#define     LED_BEAT_PIN    PB4
+
+//PLL Definitionen
+#define     R0_R_Counter        2
+#define     R0_Anti_Backlash    16
+#define     R0_LDP              20
+#define     R0_SYNC             21
+#define     R0_DLY              22
+
+#define     R1_A_Counter        2
+#define     R1_B_Counter        8
+#define     R1_CP_Gain          21
+
+#define     R2_Counter_Reset    2
+#define     R2_Power_Down_1     3
+#define     R2_MUX              4
+#define     R2_PD_Polarity      7
+#define     R2_CP_Threestate    8
+#define     R2_Fastlock         9
+#define     R2_Timer_Counter    11
+#define     R2_Current_1        15
+#define     R2_Current_2        18
+#define     R2_Power_Down_2     21
+#define     R2_Prescaler        22
+
+#define     Pres_8              0x0UL
+#define     Pres_16             0x1UL
+#define     Pres_32             0x2UL
+#define     Pres_64             0x3UL
+
+//************************************
+//PLL Parameter
+//FREQUENZ, KANALRASTER, REFERENZ
+//Frequenzen sind in Hz
+//************************************
+
+#define     Frequenz            144300000
+#define     Referenz            25000000
+#define     Step                2500
+#define     Prescaler           8
+#define     Prescaler_Reg       Pres_8
+#define     MuxPin              0b001       //MUX Parameter (010 = N Out, 001 = Digital Lock Detect, 100 = R Out)
+
+#endif
